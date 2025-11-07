90368ceb068c1d6a776c26f1b193ee9934a01f65
canersinan00 <canersinan1013@gmail.com>
2025-11-07 17:20:46 +0300

    View Component Added

--- Repositories/obj/Debug/net8.0/Repositories.AssemblyInfo.cs
+++ Repositories/obj/Debug/net8.0/Repositories.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbd0bd0e6cb25ae4705074d217c0798adeefe648")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1eb24d5d73f5b73d6865b4241f6143612e9839e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
