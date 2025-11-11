4658fb64db6bc0538920b5a307cb180c959ede29
canersinan00 <canersinan1013@gmail.com>
2025-11-11 11:53:32 +0300

    product and category relationship created

--- Entities/Models/Product.cs
+++ Entities/Models/Product.cs
@@ -6,6 +6,10 @@ public class Product
     public int ProductId { get; set; }
     [Required(ErrorMessage = "Product name is rquired.")]
     public String? ProductName { get; set; } = String.Empty;
-    [Required(ErrorMessage ="Product price is rquired.")]
+    [Required(ErrorMessage = "Product price is rquired.")]
     public decimal Price { get; set; }
+    
+    public int? CategoryId { get; set; }    // Foreign Key
+    public Category? Category { get; set; } //Navigation property
+
 }
