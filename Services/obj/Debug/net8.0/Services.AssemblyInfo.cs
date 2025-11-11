764426aefbfd6cb388fd54caa0b9162bb03022d7
canersinan00 <canersinan1013@gmail.com>
2025-11-11 17:03:32 +0300

    Cart Page Created

--- Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
+++ Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c4aadf8569d4dd7f1ab22a8a8f2c6cadcbd988f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f717af38d9e7822610d61781b19794b16eb7b16")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
