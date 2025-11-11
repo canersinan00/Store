abc7493e13f8cd18347eece62e36a2ccf2d7089f
canersinan00 <canersinan1013@gmail.com>
2025-11-11 15:19:15 +0300

    Dtos Finish

--- StoreApp/Areas/Admin/Controllers/ProductController.cs
+++ StoreApp/Areas/Admin/Controllers/ProductController.cs
@@ -24,16 +24,19 @@ namespace StoreApp.Areas.Admin.Controllers
 
         public IActionResult Create()
         {
-            ViewBag.Categories = new SelectList(
+            ViewBag.Categories = GetCategoriesSelectList();
+            return View();
+        }
+
+        private SelectList GetCategoriesSelectList()
+        {
+            return new SelectList(
                 _manager.CategoryService.
                 GetAllCategories(false),
                 "CategoryId",
                 "CategoryName",
                 "1"
                 );
-
-
-            return View();
         }
 
         [HttpPost]
@@ -50,12 +53,13 @@ namespace StoreApp.Areas.Admin.Controllers
 
         public IActionResult Update([FromRoute(Name = "id")] int id)
         {
-            var model = _manager.ProductService.GetOneProduct(id, false);
+            ViewBag.Categories = GetCategoriesSelectList();
+            var model = _manager.ProductService.GetOneProductForUpdate(id, false);
             return View(model);
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Update(Product product)
+        public IActionResult Update([FromForm]ProductDtoForUpdate product)
         {
             if (ModelState.IsValid)
             {
