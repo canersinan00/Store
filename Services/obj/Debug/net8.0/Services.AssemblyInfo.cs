1c088293ae559911d475bcecfc182aae20745227
canersinan00 <canersinan1013@gmail.com>
2025-11-13 13:03:54 +0300

    Cart model and functions created

--- Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
+++ Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f717af38d9e7822610d61781b19794b16eb7b16")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+764426aefbfd6cb388fd54caa0b9162bb03022d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
