628c134fae96acbf373248244d742dabead46406
canersinan00 <canersinan1013@gmail.com>
2025-11-19 21:10:00 +0300

    Page Link Tag Helper

--- Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
+++ Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3db3392f6e8678a0a933279e3e76aa50e6e0ec4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+115ff5385eea6ccb12a7abfc08366cd18dae0590")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
