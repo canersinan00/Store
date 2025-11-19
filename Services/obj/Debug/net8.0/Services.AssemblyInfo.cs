d6926647fc88243550dd75c0abe49ff2b147b9f6
canersinan00 <canersinan1013@gmail.com>
2025-11-19 22:25:34 +0300

    Identity Added

--- Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
+++ Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b601540f815afb632f588a64503ee4a120049c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb7bf525912119d4733d70b28f41d938fc9d3504")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
