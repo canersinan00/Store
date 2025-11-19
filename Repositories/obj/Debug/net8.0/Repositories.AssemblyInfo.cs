7b601540f815afb632f588a64503ee4a120049c2
canersinan00 <canersinan1013@gmail.com>
2025-11-19 21:52:30 +0300

    UI Update

--- Repositories/obj/Debug/net8.0/Repositories.AssemblyInfo.cs
+++ Repositories/obj/Debug/net8.0/Repositories.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+115ff5385eea6ccb12a7abfc08366cd18dae0590")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+628c134fae96acbf373248244d742dabead46406")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
