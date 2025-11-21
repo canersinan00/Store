e11f141797541c81cd40749ff4b8a1e3055856dc
canersinan00 <canersinan1013@gmail.com>
2025-11-21 12:02:08 +0300

    Login Methods

--- Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
+++ Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6926647fc88243550dd75c0abe49ff2b147b9f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c2213b6122f7e38f93d2231df8024d6d02d591e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
